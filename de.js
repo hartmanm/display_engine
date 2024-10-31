875f1946b4f73e3b8123c702661a51bc716d2a45
Michael Neill Hartman <16545449+hartmanm@users.noreply.github.com>
2024-10-31 11:14:25 -0700

    added config.js guard

--- de.js
+++ de.js
@@ -59,7 +59,10 @@ for(var key of temp){
 const contains_png = key.includes("png");
 const contains_jp = key.includes("jp");
 const contains_xyz=(contains_png || contains_jp);
-if(key != ""){if(contains_xyz){image_selector.push(key);console.log(key);}}};
+const contains_const key.includes("const");
+const contains_undefined key.includes("undefined");
+const contains_error=(contains_const || contains_undefined);
+if(key != ""){if(contains_xyz && !contains_error){image_selector.push(key);console.log(key);}}};
 console.log(base.innerHTML);
 reload();
 };
