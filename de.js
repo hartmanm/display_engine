b27e4bebab0414422b1316767a6982ab706cfd3c
Michael Neill Hartman <16545449+hartmanm@users.noreply.github.com>
2024-10-31 11:30:02 -0700

    typo

--- de.js
+++ de.js
@@ -61,7 +61,7 @@ const contains_png = key.includes("png");
 const contains_jp = key.includes("jp");
 const contains_xyz=(contains_png || contains_jp);
 const contains_cons = key.includes("cons");
-const contains_undefined key.includes("undefined");
+const contains_undefined = key.includes("undefined");
 const contains_error=(contains_cons || contains_undefined);
 if(key != ""){if(contains_xyz && !contains_error){image_selector.push(key);console.log(key);}}};
 console.log(base.innerHTML);
