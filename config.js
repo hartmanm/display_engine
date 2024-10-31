b777b610c9618c4de903b8ba09eb4d1a6b3ba878
Michael Neill Hartman <16545449+hartmanm@users.noreply.github.com>
2024-10-31 08:57:09 -0700

    added default imgs to config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+const link_page_config_copyright=`
+Copyright (c) 2018 Michael Neill Hartman. All rights reserved.
+mnh_license@proton.me
+https://github.com/hartmanm
+`
+const images_list = [
+'https://hartmanm.github.io/static/resume_site/d3_road_deaths.png',
+'https://hartmanm.github.io/static/resume_site/hs_server.png',
+'https://hartmanm.github.io/static/resume_site/link_page.png',
+'https://hartmanm.github.io/static/resume_site/lvl3.png',
+'https://hartmanm.github.io/static/resume_site/m11.png',
+'https://hartmanm.github.io/static/resume_site/mining_electric_cost_calculator.png',
+'https://hartmanm.github.io/static/resume_site/nvoc.png',
+'https://hartmanm.github.io/static/resume_site/openrig.png',
+'https://hartmanm.github.io/static/resume_site/polar.png',
+'https://hartmanm.github.io/static/resume_site/riglist.png',
+'https://hartmanm.github.io/static/resume_site/wasm_go.png'
+]
